a91445a5c6c9f7542af896e008a9e4e8a8a282fa
malathi-shetty <113874244+malathi-shetty@users.noreply.github.com>
2025-11-24 15:12:07 +0530

    Updated Main-code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
-class Test
-  {
-     static int a=90;
-    kkkggggllll
-  }
+class Test {
+    static int a = 90;
+
+    public static void main(String[] args) {
+        System.out.println("Value of a is: " + a);
+
+       System.out.println("Hello!");
+    }
+}
